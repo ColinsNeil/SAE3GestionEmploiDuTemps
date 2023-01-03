4042755a0f1cbc2e02564ddf396ecf5cd1330185
Colins Neil <colinsneilnimalaraj@gmail.com>
2023-01-03 21:13:14 +0100

    Ajout de Flight/Smarty et restructuration des dossiers

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php 
-    include('config/config.php');
-    header("location:pages/accueil.php");
+    // Importation de libraires utiles
+    require('../includes/Flight/flight/Flight.php');
+    require('../includes/Smarty/libs/Smarty.class.php');
+    require('../includes/pdo.php');
+
+    // Importation du fichier contenant tout les routes
+    require ('./routes.php');
+
+    // On demande à Flight d'utiliser Smarty pour le rendu des pages HTMLs
+    Flight::register('view', 'Smarty', array(), function($smarty){
+        $smarty->template_dir = './templates/';
+        $smarty->compile_dir = './templates_c/';
+        $smarty->config_dir = './config/';
+        $smarty->cache_dir = './cache/';
+    });
+
+    // On ajoute une méthode render à Flight qui affiche un template en lui transmettant un tableau de données
+    Flight::map('render', function($template, $data){
+        Flight::view()->assign($data);
+        Flight::view()->display($template);
+    });
+
+    /*header("location:pages/accueil.php");*/
 ?>
